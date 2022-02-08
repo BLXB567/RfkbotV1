66444e4c8e1e703b4b59b72b39a06c15746b4f57
BLXB567 <97624291+BLXB567@users.noreply.github.com>
2022-02-08 12:20:04 +0700

    Update __a.js

--- plugins/__a.js
+++ plugins/__a.js
@@ -1,8 +1,8 @@
-if video / image = m, false, { blablabla
+if video / image = m, false, { LOL
 conn.sendFile(m.chat, cita, 'asupan.mp4', 'Nih kak asupan nya!!', m, false, { contextInfo: { forwardingScore: 999, isForwarded: true }})
 m, false, { contextInfo: { forwardingScore: 999, isForwarded: true }})
 
-if button / image with button = m, { blablabla
+if button / image with button = m, { LOL
 m, { contextInfo: { forwardingScore: 999, isForwarded: true }})
 
 //@${who.split`@`[0]}        /////@${m.sender.split`@`[0]}
