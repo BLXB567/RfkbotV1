b43306b7e5a07c2fe4191c93f7e8458124cdc655
BLXB567 <97624291+BLXB567@users.noreply.github.com>
2022-02-08 12:35:59 +0700

    Update belibot.js

--- plugins/belibot.js
+++ plugins/belibot.js
@@ -1,10 +1,10 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => {
-	conn.send3ButtonLoc(m.chat, await (await fetch(fla + 'sewa bot')).buffer(), `╠═〘 Beli Bot 〙 ═
+	conn.send3ButtonLoc(m.chat, await (await fetch(fla + 'sewa bot')).buffer(), `╠═〘 Sewa bot 〙 ═
 ╠➥ *1 Bulan* :      *Rp 20.000,00*
 ╠➥ *Permanen* : *Rp 30.000,00*
 ╠➥ *Premium* :   *Rp 15.000,00*
-╠➥ *Sc Bot* :        *Masih Beta*
+╠➥ *Sc Bot* :        *cari sendiri bang di yt banyak*
 ║
 ╠═〘 PEMBAYARAN 〙 ═
 ╠➥ Gopay, Dana, Dan Pulsa
@@ -12,7 +12,7 @@ let handler = async (m, { conn }) => {
 ╠═ Tertarik Untuk Beli Bot Ini?
 ╠➥Ketuk Tombol Di Bawah Ya
 ║
-╠═ ©2021 Rpg wabot-aq
+╠═ ©2022 BLZXB5
 ╠═ Scrip original by Nurutomo
 ╠═〘 ${namabot} 〙 ═`.trim(), footer, 'Dana', '#viadana', 'OVO', '#viaovo', 'Pulsa', '#viapulsa', m)
 }
