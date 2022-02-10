29e1ebbf6e8ac8530a2a6115c9622eaeedfdb01e
BLXB567 <97624291+BLXB567@users.noreply.github.com>
2022-02-10 13:08:11 +0700

    Update tiktok.js

--- plugins/tiktok.js
+++ plugins/tiktok.js
@@ -6,7 +6,7 @@ let res = await fetch(`https://api.dapuhy.xyz/api/socialmedia/dddtik?url=${args[
 if (!res.ok) throw await res.text()
 let json = await res.json()
 if (!json.status) throw json
-let { title, download } = json.result
+let { title, download } = json.data
 await conn.sendFile(m.chat, download.server1, 'tt.mp4', `${title}`.trim(), m)
 }
 
