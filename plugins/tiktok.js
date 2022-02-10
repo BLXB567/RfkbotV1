09e71cc4507c7a0b317a5c8fe91914af1553339f
BLXB567 <97624291+BLXB567@users.noreply.github.com>
2022-02-10 13:22:17 +0700

    Update tiktok.js

--- plugins/tiktok.js
+++ plugins/tiktok.js
@@ -2,11 +2,11 @@ let fetch = require('node-fetch')
 let handler = async (m, { conn, args }) => {
 if (!args[0]) throw 'Uhm..url nya mana?'
 m.reply(wait)
-let res = await fetch(`https://api.dapuhy.xyz/api/socialmedia/dddtik?url=${args[0]}&apikey=HRHdL99iyl`)
+let res = await fetch(`https://api.dapuhy.xyz/api/socialmedia/tiktoknowm?url=${args[0]}&apikey=HRHdL99iyl`)
 if (!res.ok) throw await res.text()
 let json = await res.json()
 if (!json.status) throw json
-await conn.sendFile(m.chat, json.download.server1, 'tt.mp4', `json.title`.trim(), m)
+await conn.sendFile(m.chat, json.download.server2, 'tt.mp4', `json.title`.trim(), m)
 }
 
 handler.help = ['tiktok <url>']
